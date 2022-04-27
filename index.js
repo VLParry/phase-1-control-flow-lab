4ecf94bbfb8a71fc4d4a8f66ed4596f477aeeaa8
Victoria Parry <vlparry@gmail.com>
2022-04-26 22:33:07 -0600

    commit tree 4e75c0a3c8231b3a5b049e6efa8065df74ed9df4

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feetTraveled) {
+  if (feetTraveled <= 400) {
+    return 'This one is on me!';
+  } else if (feetTraveled <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (feetTraveled <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(cityName) {
+  return cityName === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
